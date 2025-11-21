41a31a80d393dc7321ac9a5332e36f51a663c0a9
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-11-21 12:47:32 +0100

    feat:made the add note button do at least somehting(btw it dosent add notes yet)

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,6 +3,13 @@ import { View } from "../view/view";
 class Controller {
   constructor() {
     this.view = new View();
+    this.setListener();
+  }
+  setListener() {
+    const button = this.view.main.mainBtn.getTag();
+    button.addEventListener("click", () => {
+      console.log("never gonna give you up");
+    });
   }
 }
 new Controller();
--- src/view/view-params.js
+++ src/view/view-params.js
@@ -2,5 +2,8 @@ const mainParams = {
   tagName: "main",
   classList: ["flex", "justify-center", "py-5"],
 };
+// const formParams = {
+//   tagName:"form",
+// }
 
 export { mainParams };
