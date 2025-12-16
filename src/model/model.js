ae4eaf323760c61db97184220093b0799889434d
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-12-16 12:36:34 +0100

    feat:added some functions for model

--- src/model/model.js
+++ src/model/model.js
@@ -1,7 +1,23 @@
 export class Model {
+  key = "notes";
   constructor() {
-    console.log(1);
-    localStorage.setItem("fox", "cat");
-    alert(localStorage.getItem("fox"));
+    this.getData(this.key);
+  }
+
+  addData(key, data) {
+    const DataJson = JSON.stringify(data);
+    localStorage.setItem(key, DataJson);
+  }
+  getData(key) {
+    const structure = {
+      normal: [],
+      favorite: [],
+    };
+    const keyIsInLocalStorage = localStorage.getItem(key);
+    if (!keyIsInLocalStorage) {
+      this.addData(key, structure);
+      return structure;
+    }
+    return keyIsInLocalStorage;
   }
 }
